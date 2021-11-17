c99381ba0b70123be8b952a4ec92a73eca260e0e
simonrtan <simonsimon_simon@yahoo.com>
2021-11-17 00:13:43 -0500

    - Created a basic json logic sample

--- JsonLogicConsole/Program.cs
+++ JsonLogicConsole/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using JsonLogic.Net;
+using Newtonsoft.Json;
+
+var input = new
+{
+    Name = "John Doe",
+    Age = 23,
+    IsDeveloper = false
+};
+
+var rule = File.ReadAllText(@$"sample.json");
+
+var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
+
+var result = evaluator.Apply(rule, input);
+
+if (result.IsTruthy())
+    Console.WriteLine("Truth!");
+else
+    Console.WriteLine("Lie!");
