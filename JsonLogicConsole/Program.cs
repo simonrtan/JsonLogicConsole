4f99689bb8e26f7afd32056a28f94cf9b1ec82c0
simonrtan <simonsimon_simon@yahoo.com>
2021-11-24 00:06:07 -0500

    Create RowRules Library

--- JsonLogicConsole/Program.cs
+++ JsonLogicConsole/Program.cs
@@ -1,8 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using JsonLogic.Net;
-using Newtonsoft.Json;
-
-var input = new
+﻿var input = new
 {
     Name = "John Doe",
     Age = 23,
@@ -11,11 +7,8 @@ var input = new
 
 var rule = File.ReadAllText(@$"sample.json");
 
-var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-
-var result = evaluator.Apply(rule, input);
-
-if (result.IsTruthy())
+var rules = new RowRules.RowRules();
+if (rules.EvaluateData(input, rule))
     Console.WriteLine("Truth!");
 else
     Console.WriteLine("Lie!");
---
+++ RowRules/RowRules.cs
@@ -0,0 +1,23 @@
+﻿using JsonLogic.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RowRules
+{
+    public class RowRules
+    {
+        public bool EvaluateData(object data, string rule)
+        {
+            var jObjectRule = JObject.Parse(rule);
+
+            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
+
+            var result = evaluator.Apply(jObjectRule, data);
+
+            if (result.IsTruthy())
+                return true;
+
+            return false;
+        }
+    }
+}
